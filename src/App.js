795c43ab6b457c399ebea6902cc03215190a1ead
James Tinker <jtinker@t15digitalmedia.ca>
2020-12-29 12:11:42 -0500

    Removed all firebase content to be implemented later

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './css/App.css';
 import { useContext } from 'react';
 import { Router } from '@reach/router';
-import { UserContext } from "./providers/UserProvider";
 import Home from './views/public/Home';
-import Admin from './views/private/Admin';
-import SignIn from './components/signIn/SignIn';
 
 function App() {
-  const user = useContext(UserContext);
   return (
     <>
       <Router>
         <Home path="/" />
-        {user ?
-          <Admin path="/admin/*"/>
-        : 
-          <SignIn path="/admin" />
-        } 
       </Router>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UserProvider from "./providers/UserProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
       <App />
-    </UserProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
