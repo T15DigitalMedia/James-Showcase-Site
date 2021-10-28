465d2518d0bdf83360a1501650ea58362d34c7f8
James Tinker <jtinker@t15digitalmedia.ca>
2021-10-28 00:44:49 -0400

    Updated gitlab url to github profile

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,33 +1,45 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGitlab, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-    return ( 
-        <footer className="footer bg-light">
-            <div className="container">
-                <div className="row justify-content-center text-center">
-                    <div className="col-md-12">
-                        <div className="text-center text-white footer-alt">
-                            <ul className="list-unstyled list-inline mb-3">
-                                <li className="list-inline-item">
-                                    <a href="https://www.linkedin.com/in/james-tinker/" target="_blank" rel="noreferrer">
-                                        <FontAwesomeIcon icon={faLinkedin} className="text-muted" />
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="https://gitlab.com/james_tinker" target="_blank" rel="noreferrer">
-                                        <FontAwesomeIcon icon={faGitlab} className="text-muted" />
-                                    </a>
-                                </li>
-                            </ul>
-                            <p className="text-muted mb-0"> 2020 - {(new Date().getFullYear())} &copy; T15DigitalMedia. Design by SRBThemes.</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className="footer bg-light">
+      <div className="container">
+        <div className="row justify-content-center text-center">
+          <div className="col-md-12">
+            <div className="text-center text-white footer-alt">
+              <ul className="list-unstyled list-inline mb-3">
+                <li className="list-inline-item">
+                  <a
+                    href="https://www.linkedin.com/in/james-tinker/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} className="text-muted" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="https://github.com/JamTinker"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faGithub} className="text-muted" />
+                  </a>
+                </li>
+              </ul>
+              <p className="text-muted mb-0">
+                {" "}
+                2020 - {new Date().getFullYear()} &copy; T15DigitalMedia. Design
+                by SRBThemes.
+              </p>
             </div>
-        </footer>
-    );
-}
- 
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
